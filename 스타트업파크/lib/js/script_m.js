b7c7f95ddd05e58c9ba0dbf7a88ecf2e379e68fb
DESKTOP-FG2G63J\PC033 <jangsg687040.gamil.com>
2021-06-24 16:50:11 +0900

    스타트업파크

---
+++ 스타트업파크/lib/js/script_m.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+});
+var options = {
+   horizontal: 1,
+   itemNav: 'centered', //basic , centered,
+   speed: 300,
+   smart: 1,
+   activateOn: 'click',
+   mouseDragging: 1,
+   touchDragging: 1,
+   activateMiddle: 1,
+   releaseSwing: 1,
+   // itemSelector:1
+};
+var frame = new Sly('.tabframe', options).init();
